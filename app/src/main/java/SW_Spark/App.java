c7428574050cfb604b5d8016a050aadf68f139eb
kuqifit1 <kuqifit1@students.zhaw.ch>
2022-03-15 16:16:44 +0100

    main

--- app/src/main/java/SW_Spark/App.java
+++ app/src/main/java/SW_Spark/App.java
@@ -3,12 +3,18 @@
  */
 package SW_Spark;
 
+import spark.Spark;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
-}
+        Spark.get("/welcome"
+        , (req, res) -> {
+        return "Hallo DevOps";
+        });
+        }
+        }
+        
